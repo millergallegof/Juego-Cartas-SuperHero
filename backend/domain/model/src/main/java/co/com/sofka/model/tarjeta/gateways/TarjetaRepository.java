df31b2217effa8a5d7002d6136461b8c1e7672bd
millergallegof <miller.gallegof@gmail.com>
2022-07-19 19:45:08 -0500

    Creacion Modelo Tarjeta

---
+++ backend/domain/model/src/main/java/co/com/sofka/model/tarjeta/gateways/TarjetaRepository.java
@@ -0,0 +1,10 @@
+package co.com.sofka.model.tarjeta.gateways;
+
+import co.com.sofka.model.tarjeta.Tarjeta;
+import reactor.core.publisher.Mono;
+
+public interface TarjetaRepository {
+    Mono<Tarjeta> save(Tarjeta tarjeta);
+
+
+}
