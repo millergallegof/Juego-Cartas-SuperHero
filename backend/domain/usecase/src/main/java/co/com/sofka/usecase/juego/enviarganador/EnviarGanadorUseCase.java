a0c0e18c92ac42be77d7faf59e27cf565095ab14
danielgil <danistcruz@gmail.com>
2022-07-19 23:30:31 -0500

    se hacen casos de uso

--- backend/domain/usecase/src/main/java/co/com/sofka/usecase/juego/enviarganador/EnviarGanadorUseCase.java
+++ backend/domain/usecase/src/main/java/co/com/sofka/usecase/juego/enviarganador/EnviarGanadorUseCase.java
@@ -1,7 +1,17 @@
 package co.com.sofka.usecase.juego.enviarganador;
 
+import co.com.sofka.model.juego.Juego;
+import co.com.sofka.model.juego.gateways.JuegoRepository;
 import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Mono;
+
 @RequiredArgsConstructor
 public class EnviarGanadorUseCase {
-    
+
+    private final JuegoRepository juegoRepository;
+
+    public Mono<Juego> enviarGanador(String id, Juego juego) {
+        return juegoRepository.enviarGanador(id,juego);
+    }
+
 }
