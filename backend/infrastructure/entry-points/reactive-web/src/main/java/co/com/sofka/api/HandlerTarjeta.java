66af29d7d5f07752d2a7f96243a90e6101b17f19
millergallegof <miller.gallegof@gmail.com>
2022-07-19 23:59:40 -0500

    Creacion metodos Handler Tarjeta

--- backend/infrastructure/entry-points/reactive-web/src/main/java/co/com/sofka/api/HandlerTarjeta.java
+++ backend/infrastructure/entry-points/reactive-web/src/main/java/co/com/sofka/api/HandlerTarjeta.java
@@ -1,6 +1,12 @@
 package co.com.sofka.api;
 
+import co.com.sofka.model.tarjeta.Tarjeta;
+import co.com.sofka.usecase.tarjeta.actualizartarjeta.ActualizarTarjetaUseCase;
+import co.com.sofka.usecase.tarjeta.createtarjeta.CreateTarjetaUseCase;
+import co.com.sofka.usecase.tarjeta.deletetarjeta.DeleteTarjetaUseCase;
+import co.com.sofka.usecase.tarjeta.listarcartas.ListarCartasUseCase;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
@@ -9,20 +15,37 @@ import reactor.core.publisher.Mono;
 @Component
 @RequiredArgsConstructor
 public class HandlerTarjeta {
-//private  final UseCase useCase;
-//private  final UseCase2 useCase2;
-    public Mono<ServerResponse> listenGETUseCase(ServerRequest serverRequest) {
-        // usecase.logic();
-        return ServerResponse.ok().bodyValue("");
+    private final CreateTarjetaUseCase createTarjetaUseCase;
+    private final ActualizarTarjetaUseCase actualizarTarjetaUseCase;
+    private final DeleteTarjetaUseCase deleteTarjetaUseCase;
+    private final ListarCartasUseCase listarCartasUseCase;
+
+    public Mono<ServerResponse> crearTarjetaPOSTUseCase(ServerRequest serverRequest) {
+
+        return serverRequest.bodyToMono(Tarjeta.class)
+                .flatMap(element -> ServerResponse.ok()
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(createTarjetaUseCase.crearTarjeta(element), Tarjeta.class));
     }
 
-    public Mono<ServerResponse> listenGETOtherUseCase(ServerRequest serverRequest) {
-        // useCase2.logic();
-        return ServerResponse.ok().bodyValue("");
+    public Mono<ServerResponse> eliminarTarjetaDELETEUseCase(ServerRequest serverRequest) {
+        var id = serverRequest.pathVariable("id");
+        return ServerResponse.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(deleteTarjetaUseCase.eliminarTarjeta(id), Tarjeta.class);
     }
 
-    public Mono<ServerResponse> listenPOSTUseCase(ServerRequest serverRequest) {
-        // usecase.logic();
-        return ServerResponse.ok().bodyValue("");
+    public Mono<ServerResponse> atcualizarPOSTUseCase(ServerRequest serverRequest) {
+        var id = serverRequest.pathVariable("id");
+        return serverRequest.bodyToMono(Tarjeta.class)
+                .flatMap(element -> ServerResponse.ok()
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(actualizarTarjetaUseCase.actualizarTarjeta(id, element), Tarjeta.class));
+    }
+
+    public Mono<ServerResponse> listarTarjetasGETUseCase(ServerRequest serverRequest) {
+        return ServerResponse.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(listarCartasUseCase.listarTarjetas(), Tarjeta.class);
     }
 }
