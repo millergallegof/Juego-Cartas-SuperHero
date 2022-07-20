2373c974b87b56a25a4589e48e4fd7768608f766
Hernan Velasquez <velasquezmhernan@gmail.com>
2022-07-20 00:12:28 -0500

    Agregando el handler de apostar carta y guardar jugador

--- backend/infrastructure/entry-points/reactive-web/src/main/java/co/com/sofka/api/HandlerJugador.java
+++ backend/infrastructure/entry-points/reactive-web/src/main/java/co/com/sofka/api/HandlerJugador.java
@@ -1,6 +1,10 @@
 package co.com.sofka.api;
 
+import co.com.sofka.model.jugador.Jugador;
+import co.com.sofka.usecase.jugador.apostarcarta.ApostarCartaUseCase;
+import co.com.sofka.usecase.jugador.guardarjugador.GuardarJugadorUseCase;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
@@ -9,16 +13,24 @@ import reactor.core.publisher.Mono;
 @Component
 @RequiredArgsConstructor
 public class HandlerJugador {
-//private  final UseCase useCase;
-//private  final UseCase2 useCase2;
-    public Mono<ServerResponse> listenGETUseCase(ServerRequest serverRequest) {
-        // usecase.logic();
-        return ServerResponse.ok().bodyValue("");
+private  final ApostarCartaUseCase apostarCartaUseCase;
+private  final GuardarJugadorUseCase guardarJugadorUseCase;
+    public Mono<ServerResponse> apostaCartaUseCase(ServerRequest serverRequest) {
+        var id = serverRequest.pathVariable("id");
+        return serverRequest
+                .bodyToMono(Jugador.class)
+                .flatMap(e -> ServerResponse.ok()
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(apostarCartaUseCase.apostarCarta(id, e), Jugador.class));
     }
 
-    public Mono<ServerResponse> listenGETOtherUseCase(ServerRequest serverRequest) {
-        // useCase2.logic();
-        return ServerResponse.ok().bodyValue("");
+    public Mono<ServerResponse> guardarJugadorUseCase(ServerRequest serverRequest) {
+        var id = serverRequest.pathVariable("id");
+        return serverRequest
+                .bodyToMono(Jugador.class)
+                .flatMap(element -> ServerResponse.ok()
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(guardarJugadorUseCase.save(element), Jugador.class));
     }
 
     public Mono<ServerResponse> listenPOSTUseCase(ServerRequest serverRequest) {
