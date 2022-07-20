63d6a786f3aea0cea63d08742567870ee4c7c447
danielgil <danistcruz@gmail.com>
2022-07-19 19:43:34 -0500

    se crea el modelo de juego

---
+++ backend/domain/model/src/main/java/co/com/sofka/model/juego/Juego.java
@@ -0,0 +1,8 @@
+package co.com.sofka.model.juego;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder(toBuilder = true)
+public class Juego {
+}
---
+++ backend/domain/model/src/main/java/co/com/sofka/model/juego/gateways/JuegoRepository.java
@@ -0,0 +1,4 @@
+package co.com.sofka.model.juego.gateways;
+
+public interface JuegoRepository {
+}
