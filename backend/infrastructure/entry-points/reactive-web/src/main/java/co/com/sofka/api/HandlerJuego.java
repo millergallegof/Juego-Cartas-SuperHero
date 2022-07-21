f9d081d0d29747c46a313522551178f7704a8227
millergallegof <miller.gallegof@gmail.com>
2022-07-21 14:24:55 -0500

    Ajuste modelo Tablero

--- backend/infrastructure/entry-points/reactive-web/src/main/java/co/com/sofka/api/HandlerJuego.java
+++ backend/infrastructure/entry-points/reactive-web/src/main/java/co/com/sofka/api/HandlerJuego.java
@@ -4,8 +4,6 @@ import co.com.sofka.model.juego.Juego;
 import co.com.sofka.usecase.juego.crearjuego.CrearJuegoUseCase;
 import co.com.sofka.usecase.juego.asignarganador.AsignarGanadorUseCase;
 import co.com.sofka.usecase.juego.aumentaronda.AumentaRondaUseCase;
-import co.com.sofka.usecase.juego.recibirjugadores.RecibirJugadoresUseCase;
-import co.com.sofka.usecase.tarjeta.listartarjeta.ListarTarjetasUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
@@ -20,7 +18,7 @@ public class HandlerJuego {
 
  private final AsignarGanadorUseCase asignarGanadorUseCase;
  private final AumentaRondaUseCase aumentaRondaUseCase;
- private final ListarCartasUseCase listarCartasUseCase;
+// private final ListarCartasUseCase listarCartasUseCase;
 
 
     public Mono<ServerResponse> crearJuegoPOSTUseCase(ServerRequest serverRequest) {
@@ -30,25 +28,25 @@ public class HandlerJuego {
                         .body(crearJuegoUseCase.crearJuego(e), Juego.class));
     }
 
-    public Mono<ServerResponse> asignarGanadorPOSTUseCase(ServerRequest serverRequest) {
-        var id = serverRequest.pathVariable("id");
-        return serverRequest.bodyToMono(Juego.class)
-                .flatMap(element -> ServerResponse.ok()
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .body(asignarGanadorUseCase.asignarGanador(), Juego.class));
-    }
+//    public Mono<ServerResponse> asignarGanadorPOSTUseCase(ServerRequest serverRequest) {
+//        var id = serverRequest.pathVariable("id");
+//        return serverRequest.bodyToMono(Juego.class)
+//                .flatMap(element -> ServerResponse.ok()
+//                        .contentType(MediaType.APPLICATION_JSON)
+//                        .body(asignarGanadorUseCase.asignarGanador(), Juego.class));
+//    }
 
-    public Mono<ServerResponse> aumentaRondaPOSTUseCase(ServerRequest serverRequest) {
-        var id = serverRequest.pathVariable("id");
-        return serverRequest.bodyToMono(Juego.class)
-                .flatMap(element -> ServerResponse.ok()
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .body(aumentaRondaUseCase.aumentaRonda(id, element), Juego.class));
-    }
+//    public Mono<ServerResponse> aumentaRondaPOSTUseCase(ServerRequest serverRequest) {
+//        var id = serverRequest.pathVariable("id");
+//        return serverRequest.bodyToMono(Juego.class)
+//                .flatMap(element -> ServerResponse.ok()
+//                        .contentType(MediaType.APPLICATION_JSON)
+//                        .body(aumentaRondaUseCase.aumentaRonda(id, element), Juego.class));
+//    }
 
-    public Mono<ServerResponse> listarJuegosGETUseCase(ServerRequest serverRequest) {
-        return ServerResponse.ok()
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(listarCartasUseCase.listarTarjetas() , Juego.class);
-    }
+//    public Mono<ServerResponse> listarJuegosGETUseCase(ServerRequest serverRequest) {
+//        return ServerResponse.ok()
+//                .contentType(MediaType.APPLICATION_JSON)
+//                .body(listarCartasUseCase.listarTarjetas() , Juego.class);
+//    }
 }
