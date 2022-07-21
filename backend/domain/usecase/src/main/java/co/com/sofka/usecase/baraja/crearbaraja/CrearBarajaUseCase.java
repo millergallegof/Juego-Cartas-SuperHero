0295bf9ff5d83c9ac82caef5bb107a5226a1736c
millergallegof <miller.gallegof@gmail.com>
2022-07-21 18:35:48 -0500

    Commit error

--- backend/domain/usecase/src/main/java/co/com/sofka/usecase/baraja/crearbaraja/CrearBarajaUseCase.java
+++ backend/domain/usecase/src/main/java/co/com/sofka/usecase/baraja/crearbaraja/CrearBarajaUseCase.java
@@ -5,14 +5,12 @@ import co.com.sofka.model.baraja.gateways.BarajaRepository;
 import co.com.sofka.model.tarjeta.Tarjeta;
 import co.com.sofka.model.tarjeta.gateways.TarjetaRepository;
 import lombok.RequiredArgsConstructor;
-import reactor.core.publisher.Flux;
+
 import reactor.core.publisher.Mono;
 
-import java.util.HashSet;
-import java.util.Random;
-import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.*;
+
+
 
 @RequiredArgsConstructor
 public class CrearBarajaUseCase {
@@ -21,21 +19,31 @@ public class CrearBarajaUseCase {
     private final TarjetaRepository tarjetaRepository;
 
     public Mono<Baraja> crearBaraja() {
+
+        List<Tarjeta> barajaList = new ArrayList<>();
         Set<Tarjeta> baraja = new HashSet<>();
-        var tarjetas = tarjetaRepository.findAll();
-        Stream
-                .generate(new Random()::nextInt)
-                .filter(e -> e < 9 && e > 0)
-                .distinct()
-                .limit(5)
-                        .forEach(System.out::println);
-               // .map(randon -> {
-              //      System.out.println(randon);
-               //     baraja.add(tarjetas.elementAt(randon).block());
-                //    return tarjetas.elementAt(randon).block();
-               // });
-       // tarjetas.subscribe(e-> System.out.println(e));
-        System.out.println(baraja);
+        System.out.println("baraja" + barajaList);
+        tarjetaRepository.findAll()
+                .subscribe(element -> {
+                    System.out.println(element);
+                    barajaList.add(element);
+                });
+
+        Collections.shuffle(barajaList);
+
+//        Stream
+//                .generate(new Random()::nextInt)
+//                .filter(e -> e < 9 && e > 0)
+//                .distinct()
+//                .limit(5)
+//                .forEach(System.out::println);
+        // .map(randon -> {
+        //      System.out.println(randon);
+        //     baraja.add(tarjetas.elementAt(randon).block());
+        //    return tarjetas.elementAt(randon).block();
+        // });
+        // tarjetas.subscribe(e-> System.out.println(e));
+        System.out.println(barajaList);
         return barajaRepository.save(new Baraja(baraja));
     }
 }
--- backend/infrastructure/entry-points/reactive-web/src/main/java/co/com/sofka/api/HandlerBaraja.java
+++ backend/infrastructure/entry-points/reactive-web/src/main/java/co/com/sofka/api/HandlerBaraja.java
@@ -1,14 +1,7 @@
 package co.com.sofka.api;
 
 import co.com.sofka.model.baraja.Baraja;
-import co.com.sofka.model.juego.Juego;
-import co.com.sofka.model.tarjeta.Tarjeta;
 import co.com.sofka.usecase.baraja.crearbaraja.CrearBarajaUseCase;
-import co.com.sofka.usecase.juego.asignarganador.AsignarGanadorUseCase;
-import co.com.sofka.usecase.juego.aumentaronda.AumentaRondaUseCase;
-import co.com.sofka.usecase.juego.crearjuego.CrearJuegoUseCase;
-import co.com.sofka.usecase.juego.listarjuego.ListarJuegoUseCase;
-import co.com.sofka.usecase.tarjeta.listartarjeta.ListarTarjetasUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
