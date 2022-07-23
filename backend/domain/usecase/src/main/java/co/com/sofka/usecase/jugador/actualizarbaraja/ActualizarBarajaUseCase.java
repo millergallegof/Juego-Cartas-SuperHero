12cf44eb157cc197a1025d173382c8d8abd86328
danielgil <danistcruz@gmail.com>
2022-07-22 21:36:07 -0500

    se corrige error para hacer pruebas

--- backend/domain/usecase/src/main/java/co/com/sofka/usecase/jugador/actualizarbaraja/ActualizarBarajaUseCase.java
+++ backend/domain/usecase/src/main/java/co/com/sofka/usecase/jugador/actualizarbaraja/ActualizarBarajaUseCase.java
@@ -1,16 +1,18 @@
 package co.com.sofka.usecase.jugador.actualizarbaraja;
 
 import co.com.sofka.model.baraja.Baraja;
+import co.com.sofka.model.baraja.gateways.BarajaRepository;
 import co.com.sofka.model.tarjeta.Tarjeta;
 import co.com.sofka.model.tarjeta.gateways.TarjetaRepository;
 import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RequiredArgsConstructor
 public class ActualizarBarajaUseCase {
-    private final TarjetaRepository tarjetaRepository;
+    private final BarajaRepository barajaRepository;
 
-    public Mono<Baraja> actualizarTarjeta(String id, Tarjeta tarjeta) {
-        return tarjetaRepository.update(id, tarjeta);
+    public Flux<Baraja> actualizarBaraja(String id, Baraja baraja) {
+        return barajaRepository.update();
     }
 }
